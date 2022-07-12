088ae8fe0e8ef78531b6798d4e161dd9557392dc
Tu Nombre <you@example.com>
2022-07-12 15:55:27 -0500

    first commit

---
+++ karol.py
@@ -0,0 +1,15 @@
+numero = [ 10,-5,0 ,3 ,-56 ,2 ,2.3 ,7.58 ,87, 10 ]
+"""        0  1 2  3   4   5   6    7     8   9 """
+
+print(numero[2])
+
+if numero[2] > 0:
+    print ("es mayor a 0")
+    
+if numero [2] < 0:
+    print("es menor a 0 ")
+    
+else :
+    print("es igual a 0 ")
+    
+   
